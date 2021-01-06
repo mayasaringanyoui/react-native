35c208e8e2f868cdb5819110fdd204099665cbc7
Maya Saringan <maya.saringan@youi.tv>
2021-01-06 14:47:52 -0500

    r2

--- ReactCommon/cxxreact/NativeToJsBridge.cpp
+++ ReactCommon/cxxreact/NativeToJsBridge.cpp
@@ -6,7 +6,7 @@
  */
 
 #include "NativeToJsBridge.h"
-#include <ReactCommon/callinvoker/ReactCommon/CallInvoker.h>
+#include "../callinvoker/ReactCommon/CallInvoker.h"
 #include <folly/MoveWrapper.h>
 #include <folly/json.h>
 #include <glog/logging.h>
